bdd809f4c8eac8055690abaf85cf0160b79ba83f
Habib <habibalizada@gmail.com>
2019-06-07 22:54:51 -0500

    U1-M1-Summative-Alizada-Habib

---
+++ U1-M4-Summative-Alizada-Habib/src/main/java/com/example/U1M4SummativeAlizadaHabib/U1M4SummativeAlizadaHabibApplication.java
@@ -0,0 +1,13 @@
+package com.example.U1M4SummativeAlizadaHabib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M4SummativeAlizadaHabibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M4SummativeAlizadaHabibApplication.class, args);
+	}
+
+}
